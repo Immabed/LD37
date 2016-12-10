7e9a5be3462fc08ed32ba92a4bf44579b8cb35d7
Brady C <bwjccoles@gmail.com>
2016-12-10 00:02:40 -0800

    Added basic engine and resource implementations.

---
+++ Assets/Scripts/Engine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : Subsystem {
+
+    // Max power draw
+    [SerializeField][Range(0, 10)] int maxPower;
+    // Fuel draw per unit of power per second
+    [SerializeField][Range(0,2)] float fuelEfficiency;
+    // Light years per unit of power per second
+    [SerializeField][Range(0,10)] float powerEfficiency;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
