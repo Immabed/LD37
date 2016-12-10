d4e7f7b092a911a1d84264e2b9ba6c9f5c8428b4
Brady C <bwjccoles@gmail.com>
2016-12-09 22:20:38 -0800

    Implemented player movement.
    + Player movement
    + Prototype artwork and scene
    + collision detection
    + player rotation

---
+++ Assets/DetectCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision : MonoBehaviour {
+
+    public PlayerMovement pm;
+    public Direction dir;
+
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Collidable")
+            pm.Collision(dir, true);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Collidable")
+            pm.Collision(dir, false);
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float speed;
+    bool[] isWall = new bool[4];
+    public Transform imageTransform;
+
+	// Use this for initialization
+	void Start () {
+        //rb = GetComponent<Rigidbody2D>();
+	}
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Rotation
+        if (horizontal != 0f || vertical != 0f)
+        {
+            if (horizontal > 0f && vertical > 0f)
+                imageTransform.rotation = Quaternion.Euler(0, 0, 45);
+            else if (horizontal > 0f && vertical == 0f)
+                imageTransform.rotation = Quaternion.Euler(0, 0, 0);
+            else if (horizontal < 0f && vertical == 0f)
+                imageTransform.rotation = Quaternion.Euler(0, 0, 180);
+            else if (horizontal < 0f && vertical > 0f)
+                imageTransform.rotation = Quaternion.Euler(0, 0, 135);
+            else if (horizontal == 0f && vertical > 0f)
+                imageTransform.rotation = Quaternion.Euler(0, 0, 90);
+            else if (horizontal < 0f && vertical < 0f)
+                imageTransform.rotation = Quaternion.Euler(0, 0, 225);
+            else if (horizontal == 0f && vertical < 0f)
+                imageTransform.rotation = Quaternion.Euler(0, 0, 270);
+            else if (horizontal > 0f && vertical < 0f)
+                imageTransform.rotation = Quaternion.Euler(0, 0, 315);
+        }
+
+        if (isWall[(int)Direction.RIGHT] && horizontal > 0)
+            horizontal = 0;
+        if (isWall[(int)Direction.LEFT] && horizontal < 0)
+            horizontal = 0;
+        if (isWall[(int)Direction.UP] && vertical > 0)
+            vertical = 0;
+        if (isWall[(int)Direction.DOWN] && vertical < 0)
+            vertical = 0;
+
+        rb.MovePosition(rb.position + new Vector2(horizontal, vertical).normalized * speed * Time.deltaTime);
+
+
+    }
+    public void Collision(Direction dir, bool isTrigger )
+    {
+        isWall[(int)dir] = isTrigger;
+    }
+}
+
+public enum Direction { RIGHT, UP, LEFT, DOWN }
