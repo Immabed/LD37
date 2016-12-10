50979856e466d1e87e0ad419adde357469f9675f
Brady C <bwjccoles@gmail.com>
2016-12-09 22:40:26 -0800

    Moved some files, implemented subsystem menu opening

---
+++ Assets/Scripts/Subsystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Subsystem : MonoBehaviour {
+
+    public GameObject menu;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger enter");
+        if (collision.tag == "Player")
+        {
+            menu.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Trigger exit");
+        if (collision.tag == "Player")
+        {
+            menu.SetActive(false);
+        }
+    }
+}
