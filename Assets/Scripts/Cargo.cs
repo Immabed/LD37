c774e4a003113d57502789cdffa73bf955462238
Brady C <bwjccoles@gmail.com>
2016-12-11 11:44:06 -0800

    Began implementing Cargo Systems

---
+++ Assets/Scripts/Cargo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cargo {
+
+	[SerializeField]
+	int powerNeed;
+	[SerializeField]
+	int size;
+	[SerializeField]
+	bool isTimeSensitive;
+	[SerializeField]
+	float timeLimit;
+	[SerializeField]
+	string nameOfCargo;
+
+
+
+
+	public int PowerNeed { get { return powerNeed; } }
+
+	public int Size { get { return size; } }
+
+
+}
---
+++ Assets/Scripts/CargoSystem.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CargoSystem : Subsystem {
+
+	[SerializeField]
+	int cargoCapacity;
+	int cargoUsed;
+
+	Cargo[] cargo;
+	int[] powerDistribution;
+
+	int damageToPower;
+
+	int PowerNeed {
+		get { 
+			int powerNeed = 0;
+			for (int i = 0; i < cargoUsed; i++) {
+				powerNeed += cargo [i].PowerNeed;
+				i += cargo [i].Size - 1;
+			}
+			return powerNeed;
+		}
+	}
+
+
+
+
+
+	protected override void UpdatePower ()
+	{
+		currentPowerLimit = PowerNeed - damageToPower;
+		base.UpdatePower ();
+		if (currentPower < currentPowerLimit) {
+			int powerLeft = currentPowerLimit - currentPower;
+			for (int i = 0; i < cargoUsed; i++) {
+				if (powerDistribution[i] < cargo[i].PowerNeed
+				i += cargo [i].Size - 1;
+			}
+		}
+	}
+
+
+
+	protected override IEnumerator UpdateTimer ()
+	{
+		throw new System.NotImplementedException ();
+	}
+
+	protected override void DamageSystem ()
+	{
+		throw new System.NotImplementedException ();
+	}
+}
+
+
+
+
