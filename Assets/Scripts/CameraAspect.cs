a579cb740a7de376606a2f8221f4371dc64b93dd
Brady C <bwjccoles@gmail.com>
2017-01-01 13:10:17 -0800

    Support for aspect ratios taller than 16:9

---
+++ Assets/Scripts/CameraAspect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraAspect : MonoBehaviour {
+
+	Camera cam;
+	[SerializeField]
+	float defaultAspect = 16 / 9;
+	
+	// Update is called once per frame
+	void Update () {
+		// Ensure 
+		if (cam == null)
+			cam = GetComponent<Camera>();
+		var aspect = cam.aspect;
+		if (aspect >= defaultAspect){
+			cam.orthographicSize = 5;
+		}
+		else {
+			cam.orthographicSize = (defaultAspect * 5f) / aspect;
+		}
+	}
+}
