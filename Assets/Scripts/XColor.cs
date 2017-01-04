b3b71f30fe59d8bb02138a93cceddc3f3f6c9cb9
Brady C <bwjccoles@gmail.com>
2017-01-04 11:26:31 -0800

    Fixed minor issue with XColor

--- Assets/Scripts/XColor.cs
+++ Assets/Scripts/XColor.cs
@@ -8,14 +8,13 @@ public abstract class XColor : MonoBehaviour
 {
 
 	Graphic g;
-	// Use this for initialization
-	void Start()
-	{
-		g = GetComponent<Graphic>();
-	}
 
 	public void UpdateColor(Color color)
 	{
+		if (g == null)
+			g = GetComponent<Graphic>();
 		g.color = color;
+		if (g == null)
+			Debug.LogWarning("XColor: No graphic found on " + gameObject.name + " in " + GetComponentInParent<Subsystem>().gameObject.name);
 	}
 }
